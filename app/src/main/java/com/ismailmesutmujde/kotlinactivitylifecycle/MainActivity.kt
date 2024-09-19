c1138928c087b884435d2c279f1b395810e966d0
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-19 14:15:54 +0300

    updated: life cycle

--- app/src/main/java/com/ismailmesutmujde/kotlinactivitylifecycle/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinactivitylifecycle/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.ismailmesutmujde.kotlinactivitylifecycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.e("onCreate","Executed")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart","Executed")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("onResume","Executed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("onPause","Executed")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("onStop","Executed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy","Executed")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("onRestart","Executed")
+    }
+
 }
